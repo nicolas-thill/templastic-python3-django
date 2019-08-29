97ab8b3196616e8d32de1e0fd40da847a6579827
Nicolas Thill <nicolas.thill@gmail.com>
2019-08-29 23:42:54 +0200

    Redirect from root to app
    Signed-off-by: Nicolas Thill <nicolas.thill@gmail.com>

--- src/my_site/urls.py
+++ src/my_site/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('my_app/', include('my_app.urls')),
+    path('', lambda request: redirect('my_app/', permanent=False)),
 ]
